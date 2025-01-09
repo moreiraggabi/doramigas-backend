f2bfded7de557f3a8194241e9a63e279691e60e4
Gabriela Moreira <bibica.gabriela@gmail.com>
2025-01-09 18:16:25 -0300

    add auth middleware

--- src/routes/ userRoutes.ts
+++ src/routes/ userRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import { loginUsser, registerUser } from '../controllers/userController';
+import { authenticateToken } from '../middlewares/authMiddleware';
+import { AuthenticatedRequest } from '../custom';
 
 const router = Router();
 
 
 router.post('/register', registerUser); //cadastro
 router.post('/login', loginUsser) //login
+
+// Rota protegida - Exemplo
+router.get('/profile', authenticateToken, (req: AuthenticatedRequest, res) => {
+    res.json({ message: `Bem-vindo, usuário ${req.user?.email}!` });
+});
 
 export default router;
