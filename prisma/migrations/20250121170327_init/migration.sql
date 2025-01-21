05dd29ba448f93bfdf719ba64bdbc88f6f13f211
Gabriela Moreira <bibica.gabriela@gmail.com>
2025-01-21 14:05:13 -0300

    feat: excluindo a coluna genre da entidade de dramas e mudando pra entidade de relacionamento user x drama

---
+++ prisma/migrations/20250121170327_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `genre` on the `dramas` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "dramas" DROP COLUMN "genre";
